39367092b10e39c20d050900f116b3fd9d440190
Muhammad-54107 <Muhammad.qureshi107@gmail.com>
2021-01-27 15:06:33 -0500

    gitignore

--- src/Office_Hours/Practice_01_27_2021/UpDownCastings.java
+++ src/Office_Hours/Practice_01_27_2021/UpDownCastings.java
@@ -1,4 +1,19 @@
 package Office_Hours.Practice_01_27_2021;
 
+import Day45_Exceptions.PhoneTask.Iphone;
+import Day45_Exceptions.PhoneTask.Phone;
+
 public class UpDownCastings {
+
+    public static void main(String[] args) {
+        //referance type castings
+
+        Phone phone = new Iphone("12 Pro", 1100); //upcasting
+
+        Iphone iphone = (Iphone) phone; // down casting
+
+        // Phone phone2 = (Samsung) phone ;
+    }
+
+
 }
